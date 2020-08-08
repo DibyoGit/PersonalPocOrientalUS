d7954034e47c2deed3eef72b7b5e22b944794201
794305 <794305@host.docker.internal>
2020-08-08 17:21:59 +0530

    changed path /

---
+++ src/main/java/com/ofg/deposits/products/safetydepositbox/controller/SafetyDepositBoxController.java
@@ -0,0 +1,28 @@
+package com.ofg.deposits.products.safetydepositbox.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Validated
+@RestController
+public class SafetyDepositBoxController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SafetyDepositBoxController.class);
+	@Autowired
+	RestTemplate restTemplate;
+
+	// @Autowired
+	// DepositBoxService depositBoxService;
+
+	@GetMapping("/testspring")
+	public String getOwnerDetails() {
+
+		return "simple Spring controller call works";
+
+	}
+
+}
